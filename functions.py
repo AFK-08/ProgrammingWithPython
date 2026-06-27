0cd95ff17f1272b5673e09bcdc0934704658a6f1
ahmadfarooqk <ahmadfkwj@gmail.com>
2026-06-28 00:59:19 +0500

    Function Defination

---
+++ functions.py
@@ -0,0 +1,14 @@
+print("Hello")
+print(len("Hello"))
+# len is a built in function. Function has a name and then it is followed by a set of parenthesis.
+
+# What if we want to make our own function according to our requirement or to avoid repeated work.
+
+# To make Function we start with keyword def. which is basically defines a function
+
+def myFunction():
+    print("This is my First Function in Python")
+    print("Keep Learning Ahmad. You are doing Great.")
+
+myFunction()  #Function Calling
+
