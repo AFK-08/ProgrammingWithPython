25a82180eb789ae66d0c3f5ad7e8c892a3208aaa
ahmadfarooqk <ahmadfkwj@gmail.com>
2026-06-30 22:41:48 +0500

    Working on HangmanProject

--- HangmanProject.py
+++ HangmanProject.py
@@ -1,6 +1,22 @@
-x=5
-y=9
-while x<y:
-    print("Welcome to Hangman Game.")
-    print("Do you want to play it?")
-    x+=1
+import random
+print("Welcome to the Hangman Game.")
+wordslist=["ahmad","camel","car","bike","supreme"]
+word=random.choice(wordslist)
+n=1
+while n<=len(word):
+    print("_",end=" ")
+    n+=1
+guess=input("\nGuess the letter in the word")
+wordlist=list(word)
+print(wordlist)
+n=0
+length=0
+while n<len(word):
+    if guess==wordlist[n]:
+        print(wordlist[n],end=" ")
+        length+=1
+    else:
+        print("_",end=" ")
+    n+=1
+
+
