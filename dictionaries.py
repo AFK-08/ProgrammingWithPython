9ea3f18fd303a30c268fd0dd02acbeda7f5851b5
ahmadfarooqk <ahmadfkwj@gmail.com>
2026-07-02 22:54:04 +0500

    Dictionaries and Nesting Completed

---
+++ dictionaries.py
@@ -0,0 +1,21 @@
+############Learning about Dictionaries:
+
+programming_dictionary={
+    "bug":"Error in the code",
+    "Function":"code that can be recalled",
+    "loop":"Thing that keeps repeating",
+}
+print(programming_dictionary["Function"])
+
+############Adding new entry if any
+
+programming_dictionary["new"]="This is new entry"
+print(programming_dictionary)
+
+###Creating Empty Dictionary
+empty_dictionary={}
+
+####Wiping an existing Dictionary
+programming_dictionary={}
+print(programming_dictionary)
+
