d63306c20b9247f9efbf6c33769249d141892cd6
ahmadfarooqk <ahmadfkwj@gmail.com>
2026-07-02 12:31:51 +0500

    Functions with Inputs

---
+++ Functions/functions.py
@@ -0,0 +1,61 @@
+print("Hello")
+print(len("Hello"))
+# len is a built in function. Function has a name and then it is followed by a set of parenthesis.
+
+# What if we want to make our own function according to our requirement or to avoid repeated work.
+
+# To make Function we start with keyword def. which is basically defines a function
+
+def myFunction():
+    print("This is my First Function in Python")
+    print("Keep Learning Ahmad. You are doing Great.")
+
+myFunction()  #Function Calling
+
+##Functions with Inputs
+##Arguments and Parameters
+
+## Parameter: It is the name of data or variable that contains the data that is being passed to it
+
+##Argument is the actual data that is passed to parameter
+
+##You can see in following that "name" is parameter while "Ahmad Farooq Khawaja" is argument
+
+#Example 1
+def greet(name):
+    print(f"Hello How are you {name}?")
+    print(f"How is you doing {name}!")
+
+greet("Ahmad Farooq Khawaja")
+
+##Example 2:
+def life_in_weeks(age):
+    age=90-age
+    age=age*52
+    print(f"You have {age} weeks left!")
+
+life_in_weeks(56)
+
+##Function with Multiple Inputs:
+
+def greeting(name,location):
+    print(f"Hello {name}")
+    print(f"What is it to be like in {location}")
+
+greeting("Ahmad","Pakistan")
+
+
+
+
+
+
+
+
+
+
+
+     
+
+
+
+
