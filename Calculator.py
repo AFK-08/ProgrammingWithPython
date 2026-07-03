8bc8e9b7da4bf05ac2816c07bb38e3b8ff408ce7
ahmadfarooqk <ahmadfkwj@gmail.com>
2026-07-03 22:02:43 +0500

    Blackjack CapstoneProject

---
+++ Calculator.py
@@ -0,0 +1,31 @@
+def add(n1,n2):
+    return n1+n2
+def multiply(n1,n2):
+    return n1*n2
+def divide(n1,n2):
+    return n1/n2
+def subtract(n1,n2):
+    return n1-n2
+print("Welcome to Calculator")
+num1=int(input("What is the first number: "))
+value='y'
+while value=='y':
+    operation=input("Which operation you want to perform: + - * / ")
+    num2=int(input("Enter the 2nd number: "))
+    if operation=='+':
+         result=add(num1,num2)
+    elif operation=='-':
+         result=subtract(num1,num2)
+    elif operation=="*":
+         result=multiply(num1,num2)
+    elif operation=='/':
+         result=divide(num1,num2)
+    print(f"Answer is : {result}")
+    value=input("type y to continue calculation and n for not")
+    if value=='y':
+        num1=result
+    else:
+        print("Good Buy")
+
+
+
