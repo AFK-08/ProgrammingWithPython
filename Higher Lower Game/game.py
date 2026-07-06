75f5f265c487c906ddf7d97b52fbc794b8aad128
ahmadfarooqk <ahmadfkwj@gmail.com>
2026-07-06 13:27:46 +0500

    Debugging tips

---
+++ Higher Lower Game/game.py
@@ -0,0 +1,10 @@
+import random
+import data
+import os
+def clear_screen():
+    os.system('cls' if os.name=='nt' else 'clear')
+clear_screen()
+
+print("Welcome to the Higher Lower game!")
+print(data.data[0])
+
