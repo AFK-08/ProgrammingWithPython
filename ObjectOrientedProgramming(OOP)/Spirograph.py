793e9a3870768c96bb702574f5e9d4326c24141e
ahmadfarooqk <ahmadfkwj@gmail.com>
2026-07-10 20:26:26 +0500

    Spirograph Diagram

---
+++ ObjectOrientedProgramming(OOP)/Spirograph.py
@@ -0,0 +1,36 @@
+import turtle
+from turtle import Turtle,Screen
+import random
+turtle.colormode(255)
+
+def random_color():
+    r=random.randint(0,255)
+    g=random.randint(0,255)
+    b=random.randint(0,255)
+    color=(r,g,b)
+    return color
+
+screen=Screen()
+timmy=Turtle()
+timmy.speed("fastest")
+timmy.pensize(1.2)
+
+# for _ in range(int(360/5)):
+#     timmy.color(random_color())
+#     timmy.circle(100)
+#     timmy.circle(0,extent=5)
+
+####or*****************
+
+def drawSpirograph(gap_size):
+    for _ in range(int(360/gap_size)):
+        timmy.color(random_color())
+        timmy.circle(100)
+        timmy.setheading(timmy.heading()+gap_size)
+
+drawSpirograph(3)
+
+
+
+
+screen.exitonclick()
