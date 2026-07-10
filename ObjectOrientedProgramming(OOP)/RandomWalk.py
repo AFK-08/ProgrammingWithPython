8f2f6729e28c4b477bf20e374f9aea8abd16c666
ahmadfarooqk <ahmadfkwj@gmail.com>
2026-07-10 19:43:10 +0500

    RandomWalk Turtle

--- ObjectOrientedProgramming(OOP)/RandomWalk.py
+++ ObjectOrientedProgramming(OOP)/RandomWalk.py
@@ -1,15 +1,28 @@
+### Also using concept of Tuples
+
 import turtle
 from turtle import Turtle,Screen
 import random
 
-colors=["red","green","cyan","blue","yellow","black","grey","orange","purple","magenta","maroon","aqua"]
+turtle.colormode(255)
+def random_color():
+    r=random.randint(0,255)
+    g=random.randint(0,255)
+    b=random.randint(0,255)
+    color=(r,g,b)
+    return color
+
+
+
+
 direction=[0,90,180,270]
 screen=Screen()
+
 timmy=Turtle()
 timmy.pensize(15)
 turtle.speed("fastest")
 for _ in range(200):
-    timmy.color(random.choice(colors))
+    timmy.color(random_color())
     timmy.setheading(random.choice(direction))
     timmy.forward(30)
 
