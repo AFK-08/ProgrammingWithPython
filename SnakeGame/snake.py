d4b5d75c71c13b0832cb0366857ad9e9f3c4f9fb
ahmadfarooqk <ahmadfkwj@gmail.com>
2026-07-12 22:02:53 +0500

    Controlling Snake

---
+++ SnakeGame/snake.py
@@ -0,0 +1,57 @@
+STARTING_POSITIONS =[(0,0),(-20,0),(-40,0)]
+MOVE_DISTANCE=20
+from turtle import Turtle
+
+class Snake:
+    ## IT means what should happen when we create snake object..
+    def __init__(self):
+
+        self.segments=[]
+        self.create_snake()
+        self.head=self.segments[0]
+    
+    ## Creating Snake body
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment=Turtle(shape="square")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    ## Moving the snake
+    def move(self):
+        for seg in range(len(self.segments)-1,0,-1):
+            new_x=self.segments[seg-1].xcor()
+            new_y=self.segments[seg-1].ycor()
+            self.segments[seg].goto(new_x,new_y)
+        self.head.forward(MOVE_DISTANCE)
+    
+    ## Controlling Snake
+    def up(self):
+        if self.head.heading()!=270:
+            self.head.setheading(90)
+        else:
+            pass
+    
+    def down(self):
+        if self.head.heading()!=90:
+            self.head.setheading(270)
+        else:
+            pass
+
+    def left(self):
+        if self.head.heading()!=0:
+            self.head.setheading(180)
+        else:
+            pass
+
+    def right(self):
+        if self.head.heading()!=180:
+            self.head.setheading(0)
+        else:
+            pass
+    
+
+
+
+        
