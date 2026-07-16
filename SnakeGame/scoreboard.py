bef158060909e0755e1357210413823cce512559
ahmadfarooqk <ahmadfkwj@gmail.com>
2026-07-16 17:09:40 +0500

    Added HighScore File System

--- SnakeGame/scoreboard.py
+++ SnakeGame/scoreboard.py
@@ -5,16 +5,27 @@ class Score(Turtle):
         super().__init__()
         self.penup()
         self.score=0
+        self.high_score=0
         self.goto(0,260)
         self.hideturtle()
+        file=open("data.txt")
+        self.high_score = file.read()
+        file.close()
         self.update_score()
+        
     
     def update_score(self):
-        self.write(f"Score: {self.score}",align="center",move=False,font=("Courier",20,"bold"))
+        self.write(f"Score: {self.score} High Score: {self.high_score}",  align="center",move=False,font=("Courier",20,"bold"))
     
     def game_over(self):
+        
+        if self.score >int(self.high_score):
+            self.high_score = self.score
+            with open("data.txt" ,mode= "w") as file:
+                file.write(str(self.high_score))
         self.goto(0,0)
         self.write("GAME OVER!",align="center",move=False,font=("Courier",20,"bold"))
+
 
     def increase(self):
         self.clear()
