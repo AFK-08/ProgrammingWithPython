ef7290c30f489f28201c2c6638b8b645da71503a
ahmadfarooqk <ahmadfkwj@gmail.com>
2026-07-19 12:08:53 +0500

    NATO-Phonetic Alphabets

--- NATO-Alphabets Project/main.py
+++ NATO-Alphabets Project/main.py
@@ -1,2 +1,23 @@
+import pandas
+import os
+def clear_screen():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+clear_screen()
+
+data = pandas.read_csv("./NATO-Alphabets Project/nato_phonetic_alphabet.csv")
+
+code_dictionary = {row.letter:row.code for (index,row) in data.iterrows()}
+
+user_name = input("Enter your name: ")
+
+code_list = [code_dictionary[letter.upper()] for letter in user_name]
+
+print(code_list)
+
+
+
+
+
 
 
