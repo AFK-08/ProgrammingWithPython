fccdb9170b9740862e828f2434bea4fb75053df1
ahmadfarooqk <ahmadfkwj@gmail.com>
2026-07-21 22:15:49 +0500

    Next Labels show

--- learn_tkinter.py
+++ learn_tkinter.py
@@ -8,8 +8,10 @@ window.minsize(width=500,height=500)
 ## Labels
 
 my_label = tkinter.Label(text="I am a Label",font=("Arial",15,"bold"))
-my_label.pack(side="top")
+my_label.pack(side="left")
 
+my_label = tkinter.Label(text="This is another Label ")
+my_label.pack(side="top")
 
 
 
