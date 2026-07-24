6065d25ba70d426a8f8190df91d5ad3bff181348
ahmadfarooqk <ahmadfkwj@gmail.com>
2026-07-24 22:22:11 +0500

    Working with apis and json files manipulation

--- apis.py
+++ apis.py
@@ -5,4 +5,16 @@
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
+response.raise_for_status()
+
+data = response.json()
+print(data)
+
+## Making more specifc by entering into json
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+iss_position = (longitude,latitude)
+print(iss_position)
+
+
