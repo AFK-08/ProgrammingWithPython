369930d45da3aa6e15636b09962532cc6421db49
ahmadfarooqk <ahmadfkwj@gmail.com>
2026-07-25 13:24:23 +0500

    Solidifying Api concepts

--- apis.py
+++ apis.py
@@ -17,4 +17,22 @@ latitude = data["iss_position"]["latitude"]
 iss_position = (longitude,latitude)
 print(iss_position)
 
+parameters = {
+    "lat":29.506624,
+    "lng":71.635917,
+    "formatted":0,
+}
+
+## Making 2nd SUNRISE AND SUNSET TIME API
+
+response = requests.get(url="https://api.sunrise-sunset.org/json",params=parameters)
+
+response.raise_for_status()
+data = response.json()
+sunrise_time = data["results"]["sunrise"]
+sunset_time = data["results"]["sunset"]
+
+
+print(sunrise_time)
+print(sunset_time)
 
