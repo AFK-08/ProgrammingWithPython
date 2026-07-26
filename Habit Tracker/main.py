e9c7dce4301b9388ba29d079923799ff86fef470
ahmadfarooqk <ahmadfkwj@gmail.com>
2026-07-26 15:06:55 +0500

    Made Pixels Graph and Got them.

--- Habit Tracker/main.py
+++ Habit Tracker/main.py
@@ -1,13 +1,34 @@
 import requests
 
+USERNAME = "ahmadfk"
+TOKEN = "Abcd1234ghi"
+
 pixela_endpoint = "https://pixe.la/v1/users"
 user_params = {
-    "token":"Abcd1234ghi",
-    "username":"ahmadfk",
+    "token": TOKEN,
+    "username":USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
-
 # response = requests.post(url=pixela_endpoint,json=user_params)
 # print(response.text)
+
+graph_endpoint = "https://pixe.la/v1/users/ahmadfk/graphs"
+
+graph_config = {
+    "id":"graph1",
+    "name": "Book Reading Tracker",
+    "unit": "Pages",
+    "type": "int",
+    "color": "shibafu",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
+
+# response = requests.post(url=graph_endpoint,json=graph_config,headers=headers)
+# response.raise_for_status()
+# print(response.text)
+
 
