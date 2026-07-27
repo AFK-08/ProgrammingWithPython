413c9e41aaba9d95b42f3439d80a519d1b3fa10e
ahmadfarooqk <ahmadfkwj@gmail.com>
2026-07-27 15:44:04 +0500

    Completed Data Manager part and acessed Sheet data

--- Cheap_Flight_Finder/data_manager.py
+++ Cheap_Flight_Finder/data_manager.py
@@ -1,3 +1,8 @@
+import requests
+sheety_get_endpoint = "https://api.sheety.co/cfe2f857318f60093230f0fd8fa98b22/flightDeals/prices"
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def get_data(self):
+        response = requests.get(url=sheety_get_endpoint)
+        self.sheet_data = response.json()
+        return self.sheet_data["prices"]
