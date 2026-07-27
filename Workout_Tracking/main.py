b019514a0fdcd81f54e67a37c9ac83e83b4ad3c4
ahmadfarooqk <ahmadfkwj@gmail.com>
2026-07-27 13:42:02 +0500

    Connected to Sheety API Endpoint..

--- Workout_Tracking/main.py
+++ Workout_Tracking/main.py
@@ -1,6 +1,17 @@
+import requests
+import os
+
 EXERCISE_API = "nix_live_YwwaY9B8HPU1aqmDSTS9WIigqWP69fEi"
 X_APP_ID = "app_9c220284d4724bd380bb2fb7"
-import requests
+
+from datetime import datetime
+date = datetime.now().strftime("%Y/%m/%d")
+time = datetime.now().strftime("%X")
+
+
+sheety_post_endpoint = "https://api.sheety.co/cfe2f857318f60093230f0fd8fa98b22/workoutTracking/workouts"
+
+sheety_headers = {"Authorization": "Bearer ABCDEFGHI123456789"}
 
 exercise_endpoint = "https://app.100daysofpython.dev/v1/nutrition/natural/exercise"
 
@@ -16,6 +27,22 @@ headers = {
 
 response = requests.post(url=exercise_endpoint,json=exercise_parameters,headers=headers)
 data = response.json()
-print(data["exercises"][0]["nf_calories"])
+
+calories = data["exercises"][0]["nf_calories"]
+duration = data["exercises"][0]["duration_min"]
+Exercise_name = data["exercises"][0]["name"].title()
 
 
+workout_parameters = {
+    "workout":{
+        "date": date,
+        "time" : time,
+        "exercise" : Exercise_name,
+        "duration": duration,
+        "calories": calories,
+    }
+}
+
+response = requests.post(url=sheety_post_endpoint,json=workout_parameters,headers=sheety_headers)
+
+print(response.text)
