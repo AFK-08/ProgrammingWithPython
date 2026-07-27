fa83e1a5cbf9a530d6b7219c7a6442001a0643e1
ahmadfarooqk <ahmadfkwj@gmail.com>
2026-07-27 14:09:25 +0500

    Added Records in Google Sheets/ Complete Tracking

--- Workout_Tracking/main.py
+++ Workout_Tracking/main.py
@@ -1,14 +1,16 @@
 import requests
 import os
 
+## Exercise Calculator API
 EXERCISE_API = "nix_live_YwwaY9B8HPU1aqmDSTS9WIigqWP69fEi"
 X_APP_ID = "app_9c220284d4724bd380bb2fb7"
 
+## Acessing date and time
 from datetime import datetime
 date = datetime.now().strftime("%Y/%m/%d")
 time = datetime.now().strftime("%X")
 
-
+## Sheety API and Headers for Authorization:
 sheety_post_endpoint = "https://api.sheety.co/cfe2f857318f60093230f0fd8fa98b22/workoutTracking/workouts"
 
 sheety_headers = {"Authorization": "Bearer ABCDEFGHI123456789"}
@@ -24,6 +26,7 @@ headers = {
     "x-app-key": EXERCISE_API,
 }
 
+## Generating Response from Exercise API NLP
 
 response = requests.post(url=exercise_endpoint,json=exercise_parameters,headers=headers)
 data = response.json()
@@ -32,6 +35,7 @@ calories = data["exercises"][0]["nf_calories"]
 duration = data["exercises"][0]["duration_min"]
 Exercise_name = data["exercises"][0]["name"].title()
 
+## Adding Records via Sheety API
 
 workout_parameters = {
     "workout":{
